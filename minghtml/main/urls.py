c45b90100e74cec2b244f992a67011bd392d2d5a
liaoboming <a0979498797@gmail.com>
2019-03-07 18:41:22 +0800

    3/7 -1

--- minghtml/main/urls.py
+++ minghtml/main/urls.py
@@ -5,7 +5,7 @@ from main import views
 app_name = 'main'
 urlpatterns = [
     path('', views.main, name='main'),
-    path('cyut/share483jiy0ifhvjr125/', views.cyut, name='cyut'),\
+    path('cyut/share483jiy0ifhvjr125/', views.cyut, name='cyut'),
     path('version/', views.version, name='version'),
     path('connection/', views.connection, name='connection'),
 ]
