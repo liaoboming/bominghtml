90a9232427bd1e02d0a95f0d22255d8ec9ef2012
liaoboming <44357460+liaoboming@users.noreply.github.com>
2021-03-18 15:24:28 +0800

    Update views.py

--- minghtml/main/views.py
+++ minghtml/main/views.py
@@ -18,6 +18,8 @@ def version(request):
 def connection(request):
     return render(request,'main/connection.html')
 
+def computer(request):
+    return render(request,'main/computer.html')
 
 def admin_required(func):
     def auth(request, *args, **kwargs):
